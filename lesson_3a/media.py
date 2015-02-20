7acfd1e26261c1bbfb08f3a4458778801d5aa07f
upgrade <upgrade@upserver.workgroup>
2015-02-20 11:26:06 -0200

    Play Movie Trailer done

--- lesson_3a/media.py
+++ lesson_3a/media.py
@@ -1,1 +1,13 @@
 #!/bin/env python
+
+import webbrowser
+
+class Movie():    
+    def __init__(self, movie_title, movie_storyline, poster_image, trailer_url):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_url = poster_image
+        self.trailer_url = trailer_url
+        
+    def show_trailer(self):
+        webbrowser.open(self.trailer_url)
